bfe7b8b4adc5b6d016d797b8a18551083b57eb34
waitsmart <455053456@qq.com>
2019-06-27 08:48:34 +0800

    this is a test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'test01', loadChildren: './test01/test01.module#Test01PageModule' },
 ];
 
 @NgModule({
---
+++ src/app/test01/test01.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { Test01Page } from './test01.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: Test01Page
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [Test01Page]
+})
+export class Test01PageModule {}
